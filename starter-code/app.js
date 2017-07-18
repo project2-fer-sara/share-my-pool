52131aae86be51d69e0f319e43e224f3415f7fe8
sara <sara-cg@github.com>
2017-07-18 17:22:11 +0200

    addPool y poolDetails funcionan

--- starter-code/app.js
+++ starter-code/app.js
@@ -23,7 +23,7 @@ const signup = require('./routes/signup');
 const router = express.Router();
 
 const app = express();
-
+mongoose.connect('mongodb://localhost/sharepool');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- starter-code/routes/addPool.js
+++ starter-code/routes/addPool.js
@@ -4,31 +4,43 @@ const Pool = require('../models/Pool');
 
 
 router.get('/', (req, res, next) => {
-    res.render('addPool');
+  res.render('addPool');
 });
 
 router.post('/', (req, res, next) => {
-    const newPool = new Pool ( {
-      poolAdName: req.body.poolAdName,
-      address: req.body.address,
-      numberOfGuests: req.body.numberOfGuests,
-      children: req.body.children,
-      petFriendly: req.body.petFriendly,
-      smoke: req.body.smoke,
-      location: {
-        type: "Point",
-        coordinates: [req.body.latitude, req.body.longitude]
-      }
-    });
-    console.log(newPool);
-
-    newPool.save((error, p) => {
-      if (error) {
-        next(error);
-      } else {
-        res.render('/poolDetails');
-      }
-    });
+  const newPool = new Pool({
+    poolAdName: req.body.poolAdName,
+    address: req.body.address,
+    numberOfGuests: req.body.numberOfGuests,
+    children: req.body.children,
+    petFriendly: req.body.petFriendly,
+    smoke: req.body.smoke,
+    location: {
+      type: "Point",
+      coordinates: [req.body.latitude, req.body.longitude]
+    }
   });
+  console.log(newPool);
 
-  module.exports = router;
+  newPool.save((error, Pool) => {
+    if (error) {
+      next(error);
+    } else {
+      res.redirect(`/addPool/${newPool._id}`);
+    }
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  Pool.findById(req.params.id, (error, Pool) => {
+    if (error) {
+      next(error);
+    } else {
+      res.render('poolDetails', {
+        Pool
+      });
+    }
+  });
+});
+
+module.exports = router;
