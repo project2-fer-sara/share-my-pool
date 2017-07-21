262f6ddb177d1fdd1d5ca02a1d6a2e9223af3001
sara <sara-cg@github.com>
2017-07-21 10:21:00 +0200

    profile button

--- starter-code/routes/main.js
+++ starter-code/routes/main.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 const Pool = require('../models/Pool');
 
 /* GET home page. */
 router.route('/').get((req, res, next) => {
-    res.render('main');
+    res.render('main', {User});
   });
 
 module.exports = router;
